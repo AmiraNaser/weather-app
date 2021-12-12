e7075e0200209d55a161caef9ded2d251a5008f2
Amira <meronaser78@gmail.com>
2021-12-12 02:54:20 +0200

    weather app

--- website/app.js
+++ website/app.js
@@ -4,21 +4,36 @@ const zipcode = document.getElementById('zip');
 const feelings = document.getElementById('feelings');
 const generate = document.getElementById('generate');
 //ui variables
+const day = document.getElementById('day');
 const date = document.getElementById('date');
 const temp = document.getElementById('temp');
 const content = document.getElementById('content');
+//extra variables
+const description = document.getElementById('description');
+const country = document.getElementById('country');
+const temp_max = document.getElementById('temp_max');
+const temp_min = document.getElementById('temp_min');
+const humidity = document.getElementById('humidity');
 //split API url to the API_key and API_base
-const api_key = '&appid=058cbe13a692adae3f8e744e9a8c321a&units=metric';
+const api_key = '&appid=614b5036702d44c19b8b28f220550441&units=metric';
 const api_base = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+ 1 + '.'+ d.getDate()+'.'+ d.getFullYear();
-//click event callback function
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let newDay = weekday[d.getDay()];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let newDate = month[d.getMonth()] + ' '+ d.getDate()+',  '+ d.getFullYear();
+let hour = d.toLocaleString([], { hour: '2-digit', minute: '2-digit' });
+//display current hour only on page load;
+document.getElementById('hourcontainer').innerHTML = hour;
+//click event callback function month[d.getMonth()]
 const clickFunction = (e) => {
   e.preventDefault;
   //get values user entered
   const user_zip_code = zipcode.value;
   const user_feelings = feelings.value;
+  //add zipcode to the ui
+  document.getElementById('code').innerHTML = user_zip_code;
   //check if the user entered zip code
   if (!user_zip_code ) {
     alert("Please enter valid zip code");
@@ -26,7 +41,8 @@ const clickFunction = (e) => {
   //call the getdata function only if button was clicked then
   getData(api_base, user_zip_code, api_key)
   .then((data) => {
-    postData('/addData', {date: newDate, temp: data.main.temp, feelings: user_feelings})
+    //post data to the server
+    postData('/addData', {day: newDay, date: newDate, temp: data.main.temp, temp_min: data.main.temp_min, temp_max: data.main.temp_max, humidity: data.main.humidity, country: data.sys.country, description: data.weather[0].main , feelings: user_feelings})
   }).then((updateData) => {
     updateUI();
   });
@@ -45,7 +61,7 @@ const getData = async (api_base, user_zip_code, api_key) => {
     console.log(`Caught Error ${e}`);
   }
 }
-
+//post data
 const postData = async(url = '', data= {}) => {//default call back values
  //fetch route url
   const response = await fetch(url, {
@@ -73,9 +89,32 @@ const updateUI = async () => {
   try {
     const getAllData = await request.json();
     console.log(getAllData);
+    //write down date, temp and user feelings
+    day.innerHTML = getAllData.day;
     date.innerHTML = getAllData.date;
     temp.innerHTML = getAllData.temp;
     content.innerHTML = getAllData.feelings;
+    description.innerHTML = getAllData.description;
+    humidity.innerHTML = getAllData.humidity + " " + "<span style='color: #f1c858'>%</span>"
+    temp_max.innerHTML = getAllData.temp_max + " " + "<span style='color: #f1c858'>&#176;C</span>";
+    temp_min.innerHTML = getAllData.temp_min + " " + "<span style='color: #f1c858'>&#176;C</span>";
+    country.innerHTML = getAllData.country + ", ";
+    //change image according to status
+    if (getAllData.description == "Clouds") {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Antu_weather-many-clouds.svg/128px-Antu_weather-many-clouds.svg.png\">";
+    } else if (getAllData.description == "Clear") {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Antu_weather-clouds.svg/128px-Antu_weather-clouds.svg.png\"/>";
+    }else if (getAllData.description == "Rain") {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Antu_weather-showers-scattered.svg/128px-Antu_weather-showers-scattered.svg.png\">";
+    }else if (getAllData.description == "Snow") {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/4/49/Emoji_u1f328.svg\"/>";
+    }else if (getAllData.description == "Thunderstorm") {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Antu_weather-storm-day.svg/128px-Antu_weather-storm-day.svg.png\"/>";
+    }else if (getAllData.description == "Drizzle") {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Antu_weather-snow-scattered.svg/128px-Antu_weather-snow-scattered.svg.png\"/>";
+    }else {
+      document.getElementById('descriptionImage').innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Antu_weather-mist.svg/128px-Antu_weather-mist.svg.png\"/>";
+    }
   } catch (e) {
     console.log(`Caught error ${e}`);
   }
